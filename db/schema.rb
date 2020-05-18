0b5f76ee017173237111cc4bcf12998691998e80
shou.yotsuji <basketball0420100@gmail.com>
2020-05-18 13:15:05 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_05_15_075054) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.text "status"
   end
 
